526e9b0e2b98b7b4be377b5e878b3e6b927b4d35
Xavier Centelles <xavier.centelles.ioc@gmail.com>
2025-12-12 20:19:38 +0100

    Inicialització del projecte ModaGlobal2 amb HTML, CSS, i JS

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.style.backgroundColor = '#FFD3AC';
+    console.log('Script de ModaGlobal2 carregat i fons aplicat.');
+});
